91763e971702123942813132be4572011243e330
KomeRice <golde150@gmail.com>
2023-02-11 21:16:44 +0100

    refactor: level generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,7 +9,7 @@ public class LevelGenerator : MonoBehaviour
     private GameObject _environment;
     private GameData _gameData;
     private Random _rng;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,30 +24,46 @@ public class LevelGenerator : MonoBehaviour
         var curRoom = _gameData.currentRoom.GetComponent<BaseRoomBehaviour>();
         // TODO: Prevent rooms from generating in one another
         
-        if (Input.GetKeyDown(KeyCode.P) && !curRoom.doorOutFilled)
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            if (curRoom.eligibleNextRooms.Count == 0)
-            {
-                Debug.Log("No eligible next room found");
-                return;
-            }
-
-            var choice = _rng.Next(curRoom.eligibleNextRooms.Count);
-            var obj = Instantiate(curRoom.eligibleNextRooms[choice], new Vector3(0f, -10f, 0f), 
-                new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), _environment.transform);
-            var newRoom = obj.GetComponent<BaseRoomBehaviour>();
-            var otherDoor = newRoom.doorIn.GetComponent<DoorBehavior>();
-            var thisDoor = curRoom.doorOut.GetComponent<DoorBehavior>();
-            var otherRot = otherDoor.doorOrientation;
-            var targetRot = (DoorBehavior.Orientation) (((int) thisDoor.doorOrientation + 180) % 360);
-            var rot = (DoorBehavior.Orientation) ((targetRot - otherRot + 360) % 360);
-            
-            newRoom.RotateRoom(rot);
-
-            var a = thisDoor.GetAnchor().position - otherDoor.GetAnchor().position;
-            obj.transform.Translate(a, Space.World);
-            curRoom.doorOutFilled = true;
-            otherDoor.DisableDoor();
+            RandomGenerateLevel(curRoom);
         }
+    }
+
+    private void RandomGenerateLevel(BaseRoomBehaviour rootRoom)
+    {
+        if (rootRoom.eligibleNextRooms.Count == 0)
+        {
+            Debug.Log("No eligible next room found");
+            return;
+        }
+
+        var choice = _rng.Next(rootRoom.eligibleNextRooms.Count);
+        GenerateLevel(rootRoom, rootRoom.eligibleNextRooms[choice]);
+    }
+
+    private void GenerateLevel(BaseRoomBehaviour rootRoom, GameObject nextRoom)
+    {
+        if (rootRoom.doorOutFilled)
+        {
+            Debug.Log("Room already has a room attached to its exit, not generating");
+            return;
+        }
+        
+        var obj = Instantiate(nextRoom, new Vector3(0f, -10f, 0f), 
+            new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), _environment.transform);
+        var newRoom = obj.GetComponent<BaseRoomBehaviour>();
+        var otherDoor = newRoom.doorIn.GetComponent<DoorBehavior>();
+        var thisDoor = rootRoom.doorOut.GetComponent<DoorBehavior>();
+        var otherRot = otherDoor.doorOrientation;
+        var targetRot = (DoorBehavior.Orientation) (((int) thisDoor.doorOrientation + 180) % 360);
+        var rot = (DoorBehavior.Orientation) ((targetRot - otherRot + 360) % 360);
+        
+        newRoom.RotateRoom(rot);
+
+        var a = thisDoor.GetAnchor().position - otherDoor.GetAnchor().position;
+        obj.transform.Translate(a, Space.World);
+        rootRoom.doorOutFilled = true;
+        otherDoor.DisableDoor();
     }
 }
