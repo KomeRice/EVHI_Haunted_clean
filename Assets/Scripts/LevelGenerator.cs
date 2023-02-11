819df4331b1a4bfd6f4c9e4daf1a9d4884d4e63b
KomeRice <golde150@gmail.com>
2023-02-11 18:00:33 +0100

    fix: generate rooms from current room's doorOut, linking it with new room's doorIn

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -31,19 +31,18 @@ public class LevelGenerator : MonoBehaviour
 
             var obj = Instantiate(curRoom.eligibleNextRooms.First(), new Vector3(0f, -10f, 0f), 
                 new Quaternion(0.0f, 0.0f, 0.0f, 0.0f), _environment.transform);
-            var otherDoor = obj.GetComponent<BaseRoomBehaviour>().doorOut.GetComponent<DoorBehavior>();
-            var thisDoor = curRoom.doorIn.GetComponent<DoorBehavior>();
+            var newRoom = obj.GetComponent<BaseRoomBehaviour>();
+            var otherDoor = newRoom.doorIn.GetComponent<DoorBehavior>();
+            var thisDoor = curRoom.doorOut.GetComponent<DoorBehavior>();
             var otherRot = otherDoor.doorOrientation;
             var targetRot = (DoorBehavior.Orientation) (((int) thisDoor.doorOrientation + 180) % 360);
             var rot = (DoorBehavior.Orientation) ((targetRot - otherRot + 360) % 360);
             
-            Debug.Log($"Source door: {thisDoor.doorOrientation}, Other door: {otherDoor.doorOrientation}, target: {targetRot}, rotate by {rot}");
-            obj.GetComponent<BaseRoomBehaviour>().RotateRoom(rot);
+            newRoom.RotateRoom(rot);
 
             var a = thisDoor.GetAnchor().position - otherDoor.GetAnchor().position;
             obj.transform.Translate(a, Space.World);
-            Debug.Log(a);
-            curRoom.doorInFilled = true;
+            curRoom.doorOutFilled = true;
             otherDoor.DisableDoor();
         }
     }
